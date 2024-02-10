278914ff1a54d37e6f413dd62e9d433c8cc61026
Milton Toalombo <sakuraochoa@live.com>
2024-02-10 14:58:32 -0500

    añadiendo react hook

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -2,7 +2,7 @@ import Paciente from "./Paciente"
 
 export default function ListadoPacientes() {
   return (
-    <div className="w-1/2 lg:w-3/5">
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
       <h2 className="font-black text-3xl text-center">ListadoPacientes</h2>
       <p className="text-xl mt-5 mb-10 text-center">
         Administra tus {''}
--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,7 +1,7 @@
  
 export default function Paciente() {
   return (
-    <div className="m-3 bg-white shadow-md px5 py-10 rounded-xl">
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
         <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
           <span className="font-normal normal-case">Hook</span>
         </p>
